ec7a665933208119d06051f62934cf4c457918e7
aleksik <aleksi.kaistinen@allu.io>
2017-07-11 08:51:52 +0300

    clean up

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
 require('dotenv').config();
 
 const express = require('express');
-const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+let db;
 
-MongoClient.connect(process.env.MONGODB_URI, (error, database) => {
-
-  if (error) {
-    throw new Error(error.message);
-  }
-
-  app.get('/places', (request, response) => {
-
-    database.collection('places')
-      .find()
-      .toArray()
-      .then(places => {
-        return response.send(places);
-      })
-      .catch(err => {
-        console.error(err);
-        return response.sendStatus(500);
-      });
-
-  });
-  
+MongoClient.connect(process.env.MONGODB_URI, (err, database) => {
+  if (err) return console.log(err);
+  db = database;
 });
 
-app.listen(9000, _ => console.log('Listening on 9000...'));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.json({ status: 'oukkidoukki' });
+});
+
+app.get('/places', (req, res) => {
+  db.collection('places').find().toArray()
+    .then(places => res.send(places))
+    .catch(err => console.log(err));
+});
+
+const port = process.env.PORT || 9000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
